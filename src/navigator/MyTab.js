2f7ac9a506eafa6344bf38de57ea7636a6f0f804
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-01-11 14:12:29 +0700

    menambahkan beberapa assets, memperbaiki bottom tab, dan menambahkan title pada home screen

--- src/navigator/MyTab.js
+++ src/navigator/MyTab.js
@@ -1,9 +1,26 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native";
+import drink from "../assets/icon/drink.png";
+import menu from "../assets/icon/menu.png";
+import myfood from "../assets/icon/myfood.png";
 import Drink from "../screen/Drink";
 import MyFood from "../screen/MyFood";
 import HomeStack from "./HomeStack";
 
 const Tab = createBottomTabNavigator();
+
+const Icon = ({ iconUrl }) => {
+    return (
+        <Image
+            source={iconUrl}
+            resizeMode="contain"
+            style={{
+                width: 20,
+                height: 20,
+            }}
+        />
+    );
+};
 
 export default function MyTab() {
     return (
@@ -11,18 +28,28 @@ export default function MyTab() {
             initialRouteName="HomeStack"
             screenOptions={{
                 headerShown: false,
+                tabBarShowLabel: false,
             }}>
             <Tab.Screen
                 name="HomeStack"
                 component={HomeStack}
+                options={{
+                    tabBarIcon: ({}) => <Icon iconUrl={menu} />,
+                }}
             />
             <Tab.Screen
                 name="Drink"
                 component={Drink}
+                options={{
+                    tabBarIcon: ({}) => <Icon iconUrl={drink} />,
+                }}
             />
             <Tab.Screen
                 name="MyFood"
                 component={MyFood}
+                options={{
+                    tabBarIcon: ({}) => <Icon iconUrl={myfood} />,
+                }}
             />
         </Tab.Navigator>
     );
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar, SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Find Your</Text>
+            <Text style={styles.title}>Food</Text>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: StatusBar.currentHeight + 50,
+        backgroundColor: "#FF4545",
+        flex: 1,
+    },
+    title: {
+        color: "#fff",
+        fontSize: 40,
+        paddingHorizontal: 30,
+    },
+});
