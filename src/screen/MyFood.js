61e5bceda9a936d81fe13b1ef96aa6d759292792
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-01-11 13:44:35 +0700

    menambahkan file untuk folder screen

---
+++ src/screen/MyFood.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MyFood = () => {
+    return (
+        <View>
+            <Text>MyFood</Text>
+        </View>
+    );
+};
+
+export default MyFood;
